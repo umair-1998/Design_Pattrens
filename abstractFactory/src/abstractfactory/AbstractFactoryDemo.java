a3e721587d2a80ab3c02b64e90768528bc6455f5
Muhammad Umair <115793262+umair-1998@users.noreply.github.com>
2024-05-06 12:21:30 +0500

    Update AbstractFactoryDemo.java

--- abstractFactory/src/abstractfactory/AbstractFactoryDemo.java
+++ abstractFactory/src/abstractfactory/AbstractFactoryDemo.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package abstractfactory;
 
 /**
@@ -11,9 +7,6 @@ package abstractfactory;
  */
 public class AbstractFactoryDemo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         AbstractFactory shapeFactory = FactoryProducer.getFactory("DOUBLEROUNDED");
       //get an object of Shape Rectangle
