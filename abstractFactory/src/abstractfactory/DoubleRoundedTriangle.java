a653de0315a5598da7b31c97ea8528d1ecf34c59
Muhammad Umair <115793262+umair-1998@users.noreply.github.com>
2024-05-06 12:24:21 +0500

    Update DoubleRoundedTriangle.java

--- abstractFactory/src/abstractfactory/DoubleRoundedTriangle.java
+++ abstractFactory/src/abstractfactory/DoubleRoundedTriangle.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package abstractfactory;
 
 /**
  *
- * @author SP21-BSE-017
+ * @author SP21-BSE-058
  */
 public class DoubleRoundedTriangle implements Shape {
    @Override
